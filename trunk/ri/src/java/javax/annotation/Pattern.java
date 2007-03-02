796f0da9506e45d91305ba57c913bc1fb785a60f
bill.pugh <bill.pugh@879a548e-0c29-0410-a934-d95ed551ba1e>
2007-03-02 01:26:56 +0000

    Initial import.

---
+++ trunk/ri/src/java/javax/annotation/Pattern.java
@@ -0,0 +1,27 @@
+package javax.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.annotation.meta.Qualifier;
+import javax.annotation.meta.QualifierChecker;
+import javax.annotation.meta.When;
+
+@Documented
+@Qualifier
+public @interface Pattern {
+	@RegEx String value();
+    ElementType[] applyTo() default {};
+	
+	static class Checker implements QualifierChecker<Pattern> {
+
+		public When forConstantValue(Pattern annotation, Object value) {
+			if (java.util.regex.Pattern.matches(annotation.value(), (String)value))
+				return When.ALWAYS;
+			return When.MAYBE_NOT;
+		}
+		
+	}
+}
