8d13d394647aafb8498b427cac11272a213be3b0
bill.pugh <bill.pugh@879a548e-0c29-0410-a934-d95ed551ba1e>
2007-09-24 14:46:28 +0000

    rename to avoid name class with regex.Pattern

--- ri/src/main/java/javax/annotation/MatchesPattern.java
+++ ri/src/main/java/javax/annotation/MatchesPattern.java
@@ -3,6 +3,7 @@ package javax.annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.regex.Pattern;
 
 import javax.annotation.meta.TypeQualifier;
 import javax.annotation.meta.TypeQualifierValidator;
@@ -11,13 +12,13 @@ import javax.annotation.meta.When;
 @Documented
 @TypeQualifier(applicableTo=String.class)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Pattern {
+public @interface MatchesPattern {
 	@RegEx String value();
 	int flags() default 0;
 	
-	static class Checker implements TypeQualifierValidator<Pattern> {
-		public When forConstantValue(Pattern annotation, Object value) {
-			java.util.regex.Pattern p = java.util.regex.Pattern.compile(annotation.value(), annotation.flags());
+	static class Checker implements TypeQualifierValidator<MatchesPattern> {
+		public When forConstantValue(MatchesPattern annotation, Object value) {
+			Pattern p = Pattern.compile(annotation.value(), annotation.flags());
 			if (p.matcher(((String)value)).matches())
 				return When.ALWAYS;
 			return When.NEVER;
